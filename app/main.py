0fd1037d292bab393c04ab4c0f7ae2472cd05044
JcbBrtn <42615937+JcbBrtn@users.noreply.github.com>
2021-12-28 13:44:21 -0500

    Updated imports

--- app/main.py
+++ app/main.py
@@ -7,9 +7,6 @@ from tensorflow.keras.losses import sparse_categorical_crossentropy
 
 import random
 import numpy as np
-import os
-
-print(os.getcwd())
 
 vocab = [' ', '*', ',', '-', '.', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '?', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'Y', 'Z', '_', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 char_to_ind = {char:ind for ind,char in enumerate(vocab)}
